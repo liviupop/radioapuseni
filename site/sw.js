3f272ab89c8f206511665dcab431e82754a2c59f
Liviu Pop <liviu.o.pop@gmail.com>
2026-07-27 20:14:59 +0300

    Focus the landscape on the hiker

--- site/sw.js
+++ site/sw.js
@@ -4,7 +4,7 @@
    podcast/episoadele audio — acelea sunt fie live, fie stau pe alt domeniu
    (serverul AzuraCast), și trebuie cerute mereu proaspăt din rețea. */
 
-const SHELL_CACHE = "radio-apuseni-shell-v21";
+const SHELL_CACHE = "radio-apuseni-shell-v22";
 
 const SHELL_ASSETS = [
   "./",
